7a919f6d60ac2f3abf28db62d587790b3f74cc3a
Light Chinaka <chinakalight@googlemail.com>
2024-08-07 09:54:16 -0500

    wip

---
+++ src/data-source.ts
@@ -0,0 +1,17 @@
+import "reflect-metadata";
+import { DataSource } from "typeorm";
+
+export const AppDataSource = new DataSource({
+    type: "postgres",
+    host: process.env.PGHOST || 'localhost',
+    port: 5432,
+    username: process.env.POSTGRES_USER,
+    password: process.env.POSTGRES_PASSWORD,
+    database: process.env.POSTGRES_DB,
+    synchronize: true,
+    logging: false,
+    ssl: true,
+    entities: [
+    ],
+    migrations: [],
+  });
